7ea9df8dd1cc547ca317a1db063d23e1c18cb3d3
Karen Dickenson <Karen@kldwebdesign.com>
2019-07-18 15:07:11 -0400

    KLD: adding _s theme as a base

--- web/wp-content/themes/brand/postcss.config.js
+++ web/wp-content/themes/brand/postcss.config.js
@@ -1,14 +1,14 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
+const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
-    plugins: [
-        require("postcss-import"),
-        require('tailwindcss'),
-        purgecss({
-            content: ["./templates/*.php"]
-        }),
-        require("postcss-preset-env")({stage: 1}),
-        require("autoprefixer"),
-        require("cssnano")
-    ]
+	plugins: [
+		require("postcss-import"),
+		require("tailwindcss"),
+		purgecss({
+			content: ["./**/*.php"]
+		}),
+		require("postcss-preset-env")({stage: 1}),
+		require("autoprefixer"),
+		require("cssnano")
+	]
 };
