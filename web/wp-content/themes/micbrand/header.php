dc867dfedfc91e2816a2736910c0b947e5711d50
Karen Dickenson <kldinteractive@gmail.com>
2019-08-02 15:05:47 +0000

    KLD: header, footer, sidebar, kitchen sink with neccessary template, functions and script updates

--- web/wp-content/themes/micbrand/header.php
+++ web/wp-content/themes/micbrand/header.php
@@ -21,12 +21,17 @@
 </head>
 
 <body <?php body_class('bg-gray-200'); ?>>
-<div id="page" class="site">
+<div id="page" class="relative site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'micbrand' ); ?></a>
 
-	<header id="masthead" class="site-header bg-umblue border-b-2 border-gray-200 border-solid">
-		<div class="container mx-auto relative">
-			<div class="logo p-5 text-center md:pb-0 md:m-0 md:w-1/2 md:text-left "><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="/wp-content/themes/micbrand/img/VPComm_Logo_header.svg'?>" alt="Vice President for Communications, University of Michigan" class="logo-image mx-auto md:m-0" /></a></div>
+	<header id="masthead" class="site-header bg-umblue relative">
+		<div class="container mx-auto relative logo-title-search">
+			<button id="hamburger" class="menu-toggle hamburger" aria-controls="primary-menu" aria-expanded="false"><?php //esc_html_e( 'Primary Menu', 'micbrand' ); ?>
+				<span class="hamburger-box">
+					<span class="hamburger-inner"></span>
+				</span>
+			</button>
+			<div class="logo inline-block p-3 md:p-5 text-center md:pb-0 md:m-0 md:w-1/2 md:text-left"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="/wp-content/themes/micbrand/img/VPComm_Logo_header.svg'?>" alt="Vice President for Communications, University of Michigan" class="logo-image mx-auto md:m-0" /></a></div>
 			<div class="site-branding py-2 mx-auto md:text-right md:m-0 md:mr-5 md:p-0">
 				<?php
 				if ( is_front_page() && is_home() ) :
@@ -38,17 +43,18 @@
 			</div><!-- .site-branding -->
 			<div class="search mx-auto py-2 text-center md:absolute md:right-0 md:top-0 md:pt-5"><?php get_search_form(); ?></div>
 		</div>
-		<div class="container mx-auto relative">
-			<nav id="site-navigation" class="main-navigation w-full p-5 pt-0 pb-0">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'micbrand' ); ?></button>
-				<?php wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-					'menu_class'     => 'flex flex-no-wrap justify-between item-stretch content-center py-2 text-white text-sm',
-					'depth'          => 1,
-				) ); ?>
+		<div id="site-navigation" class="container mx-auto main-nav">
+				<nav class="main-navigation lg:p-5 lg:pt-0 lg:pb-0 relative">
+					<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php //esc_html_e( 'Primary Menu', 'micbrand' ); ?></button> -->
+					<?php wp_nav_menu( array(
+						'theme_location' => 'header-menu',
+						'menu_id'        => 'primary-menu',
+						'menu_class'     => 'lg:flex-no-wrap lg:justify-between lg:items-stretch lg:content-center bg-umblue lg:bg-transparent text-white',
+						'depth'          => 1,
+					) ); ?>
 			</nav><!-- #site-navigation -->
+
 		</div>
 	</header><!-- #masthead -->
-
-	<div id="content" class="site-content container mx-auto px-4">
+	<div id="hamburger-fade" class="absolute inset-0 bg-umblue opacity-75 hamburger-fade"></div>
+	<div id="content" class="site-content container mx-auto px-4 pt-2">
--- web/wp-content/themes/micbrand/js/src/app.js
+++ web/wp-content/themes/micbrand/js/src/app.js
@@ -1,1 +1,40 @@
 // Test to see if Babel is working
+
+var hamburger = document.getElementById("hamburger");
+var primaryNavigation = document.getElementById("site-navigation");
+var pageWraper = document.querySelector(".site");
+var fade = document.getElementById("hamburger-fade");
+
+fade.classList.toggle("hidden");
+
+hamburger.addEventListener("click", function() {
+	primaryNavigation.classList.toggle("open");
+	pageWraper.classList.toggle("menu-open");
+	fade.classList.toggle("hidden");
+});
+fade.addEventListener("click", function() {
+	primaryNavigation.classList.toggle("open");
+	pageWraper.classList.toggle("menu-open");
+	fade.classList.toggle("hidden");
+});
+
+var _loop = function _loop(i) {
+	accordions[i].addEventListener("click", function(event) {
+		if (!accordions[i].hasAttribute("open")) {
+			event.preventDefault();
+		} // Close all accordions.
+
+		closeAccordions();
+		accordions[i].setAttribute("open", "");
+	});
+};
+
+for (var i = 0; i < accordions.length; i++) {
+	_loop(i);
+}
+
+var closeAccordions = function closeAccordions() {
+	for (var i = 0; i < accordions.length; i++) {
+		accordions[i].removeAttribute("open");
+	}
+};
