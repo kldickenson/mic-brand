0294a05fee8e12aad14d29ae3b029f6964e59652
Karen Dickenson <kldinteractive@gmail.com>
2019-09-10 12:45:50 -0400

    KLD: making sure mic-block plugin is up to date

--- web/wp-content/plugins/mic-block/src/card/block.js
+++ web/wp-content/plugins/mic-block/src/card/block.js
@@ -33,30 +33,42 @@ registerBlockType( 'mic/mic-card', {
 	icon: 'index-card', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'mic-blocks', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	attributes: {
-		title: {
+		cardTitle: {
 			source: 'text',
 			selector: '.card__title',
 		},
-		body: {
+		cardBody: {
 			type: 'array',
 			source: 'text',
 			selector: '.card__body',
 		},
-		linkUrl: {
+		cardLinkUrl: {
 			source: 'text',
 			selector: 'a',
 			attribute: 'html',
 		},
-		imageUrl: {
+		cardImageUrl: {
 			attribute: 'src',
 			selector: '.card__image',
 		},
-		imageAlt: {
+		cardImageAlt: {
 			attributes: 'alt',
 			selector: '.card__image',
 		},
 	},
 	edit( { attributes, className, setAttributes } ) {
+		const onChangeCardTitle = newCardTitle => {
+			setAttributes( { cardTitle: newCardTitle } );
+		};
+
+		const onChangeCardLinkUrl = newCardLinkUrl => {
+			setAttributes( { cardLinkUrl: newCardLinkUrl } );
+		};
+
+		const onChangeCardBody = newCardBody => {
+			setAttributes( { cardBody: newCardBody } );
+		};
+
 		const getImageButton = openEvent => {
 			if ( attributes.imageUrl ) {
 				return (
@@ -77,34 +89,49 @@ registerBlockType( 'mic/mic-card', {
 		};
 
 		return (
-			<div className="container">
-				<PlainText
-					onChange={ content => setAttributes( { title: content } ) }
-					value={ attributes.title }
-					placeholder="Your card title"
-					className="heading"
-				/>
-				<PlainText
-					onChange={ content => setAttributes( { linkUrl: content } ) }
-					value={ attributes.linkUrl }
-					placeholder="Your card link URL"
-				/>
-				<RichText
-					onChange={ content => setAttributes( { body: content } ) }
-					value={ attributes.description }
-					placeholder="Your card text"
-				/>
-				<MediaUpload
-					onSelect={ media => {
-						setAttributes( {
-							imageAlt: media.alt,
-							imageUrl: media.url,
-						} );
-					} }
-					type="image"
-					value={ attributes.imageID }
-					render={ ( { open } ) => getImageButton( open ) }
-				/>
+			<div className={ className }>
+				<div className="url">
+					<PlainText
+						onChange={ onChangeCardLinkUrl }
+						value={ attributes.cardLinkUrl }
+						placeholder="Your card link URL"
+					/>
+				</div>
+				<div className="card bg-white flex w-100">
+					<div className="card__content text w-7/12 h-full">
+						<h3 className="card__title text-umblue p-4 py-6 md:text-xl">
+							<PlainText
+								onChange={ onChangeCardTitle }
+								value={ attributes.cardTitle }
+								placeholder="Your card title"
+								className="heading"
+							/>
+						</h3>
+						<p className="card__body p-8 px-12 md:px-6 my-4 text-xl md:text-lg">
+							<RichText
+								onChange={ onChangeCardBody }
+								value={ attributes.cardBody }
+								placeholder="Your card text"
+							/>
+						</p>
+					</div>
+					<div
+						className="card__image w-5/12 h-auto"
+						src="#"
+						alt="{ alt }"
+					/>
+					<MediaUpload
+						onSelect={ media => {
+							setAttributes( {
+								imageAlt: media.alt,
+								imageUrl: media.url,
+							} );
+						} }
+						type="image"
+						value={ attributes.imageID }
+						render={ ( { open } ) => getImageButton( open ) }
+					/>
+				</div>
 			</div>
 		);
 	},
@@ -113,44 +140,44 @@ registerBlockType( 'mic/mic-card', {
 			if ( ! src ) {
 				return (
 					<img
-						className="card__image w-5/12 h-auto"
-						src="http://fpoimg.com/300x250"
+						className="card__image"
+						src="http://fpoimg.com/364x250"
 						alt=""
 					/>
 				);
 			}
 			if ( alt ) {
 				return (
-					<img
-						className="card__image w-5/12 h-auto"
-						src={ src }
-						alt={ alt }
-					/>
+					<img className="card__image" src={ src } alt={ alt } />
 				);
 			}
 			// No alt set, so let's hide it from screen readers
 			return (
 				<img
-					className="card__image w-5/12 h-auto"
+					className="card__image"
 					src={ src }
 					alt=""
-					aria-hidden="true"
+					ariaHidden="true"
 				/>
 			);
 		};
 		return (
 			<div className="card bg-white flex w-100">
 				<div className="card__content text w-7/12 h-full">
-					<a href={ attributes.linkUrl }>
-						<h3 className="card__title text-umblue p-4 py-6 md:text-xl">
-							<a href={ attributes.linkUrl }>{ attributes.title }</a>
-						</h3>
-						<p className="card__body p-8 px-12 md:px-6 my-4 text-xl md:text-lg">
-							<a href={ attributes.linkUrl }>{ attributes.body }</a>
-						</p>
-					</a>
+					<h3 className="card__title text-umblue p-4 py-6 md:text-xl">
+						<a href={ attributes.cardLinkUrl }>
+							<RichText.Content value={ attributes.cardTitle } />
+						</a>
+					</h3>
+					<p className="card__body p-8 px-12 md:px-6 my-4 text-xl md:text-lg">
+						<a href={ attributes.linkUrl }>
+							<RichText.Content value={ attributes.cardBody } />
+						</a>
+					</p>
 				</div>
-				{ cardImage( attributes.imageUrl, attributes.imageAlt ) }
+				<div className="w-5/12 h-auto">
+					{ cardImage( attributes.cardImageUrl, attributes.cardImageAlt ) }
+				</div>
 			</div>
 		);
 	},
