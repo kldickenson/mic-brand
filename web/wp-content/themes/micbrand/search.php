a69650367586061748d06ee9e6bd195a1f7deb18
Karen Dickenson <kldinteractive@gmail.com>
2019-10-29 18:17:01 +0000

    KLD: updating the search results page and styling including mic-block cards

--- web/wp-content/themes/micbrand/search.php
+++ web/wp-content/themes/micbrand/search.php
@@ -10,48 +10,50 @@
 get_header();
 ?>
 
-<div id="content" class="site-content container mx-auto px-4 pt-2">
-	<div id="primary" class="content-area ">
+<div id="content" class="site-content container mx-auto px-4 pt-2 flex flex-wrap md:flex-none">
+	<div id="primary" class="content-area w-full order-2 ">
 		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
+				<h2 class="page-title">
 					<?php
 					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'micbrand' ), '<span>' . get_search_query() . '</span>' );
+					printf( esc_html__( 'Search Results for: %s', 'micbrand' ), '<br /><span>' . get_search_query() . '</span>' );
 					?>
-				</h1>
+				</h2>
 			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
-				the_post();
+				the_post(); ?>
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+				<div class="mb-4 pb-4 border-b">
+                <a href="<?php echo get_permalink(); ?>">
+                    <h3><?php echo get_the_title(); ?></h3>
+                </a>
+                <p><?php echo get_the_excerpt(); ?></p>
+				</div>
 
-			endwhile;
 
-			the_posts_navigation();
+			<?php endwhile; ?>
+			<?php the_posts_pagination( array(
+				'mid_size'  => 2,
+				'prev_text' => __( 'Previous', 'obp' ),
+				'next_text' => __( 'Next', 'obp' ),
+			) ); ?>
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+		<?php else: ?>
+			<h1>No results found.</h1>
+			<p>Please modify your search and try again.</p>
+		<?php endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 
 </div><!-- #content -->
 
